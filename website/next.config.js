fa67d5f987ac7dd2ecd561956241c1022c6af72e
Mahmoud Nasr <42423651+c7nasr@users.noreply.github.com>
2022-08-23 17:41:05 +0200

    fix nextpwa

--- website/next.config.js
+++ website/next.config.js
@@ -1,19 +1,16 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-        sw: 'service-worker.js',
-        buildExcludes: [/middleware-manifest.json$/]
+	pwa: {
+		dest: "public",
+		register: true,
+		skipWaiting: true,
+		disable: process.env.NODE_ENV === "development",
+		sw: 'service-worker.js',
+		buildExcludes: [/middleware-manifest.json$/]
 
-
-  },
-  i18n: {
-    locales: ["ar"],
-    defaultLocale: "ar",
-  },
+	}, i18n: {
+		locales: ["ar"], defaultLocale: "ar",
+	},
 
 });
