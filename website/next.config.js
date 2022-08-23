7bb48bdddadb8831e96eae6f10bd9de41b5df1fe
Mahmoud Nasr <42423651+c7nasr@users.noreply.github.com>
2022-08-23 17:50:13 +0200

    Update next.config.js

--- website/next.config.js
+++ website/next.config.js
@@ -8,8 +8,8 @@ const nextConfig = {
 		register: true,
 		skipWaiting: true,
 		disable: process.env.NODE_ENV === "development",
-		sw: 'service-worker.js',
-		buildExcludes: [/middleware-manifest.json$/]
+		// sw: 'service-worker.js',
+		// buildExcludes: [/middleware-manifest.json$/]
 
 	}, i18n: {
 		locales: ["ar"], defaultLocale: "ar",
