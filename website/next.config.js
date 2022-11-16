180268eee60d9f1f71941a5e95b8888d526091b4
Mahmoud Nasr <m9nasr@outlook.com>
2022-11-16 11:04:06 +0200

    Fix Deployment

--- website/next.config.js
+++ website/next.config.js
@@ -1,18 +1,20 @@
 /**
  * @type {import('next').NextConfig}
  */
-const withPWA = require("next-pwa");
 const nextConfig = {
-	pwa: {
-		dest: "public",
-		register: true,
-		skipWaiting: true,
-		disable: process.env.NODE_ENV === "development",
-		// sw: 'service-worker.js',
-		// buildExcludes: [/middleware-manifest.json$/]
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
 
-	}, i18n: {
-		locales: ["ar"], defaultLocale: "ar",
-	},
-}
-module.exports = withPWA(nextConfig)
+  i18n: {
+    locales: ["ar"],
+    defaultLocale: "ar",
+  },
+};
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development',
+	register: true,
+  });
+  module.exports = withPWA(nextConfig);
